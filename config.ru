fb99bdc7b3786b2b9826dd2261a6c57091898bb5
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-12-15 00:51:27 -0500

    ready for first deployment

---
+++ config.ru
@@ -0,0 +1,2 @@
+require 'data'
+run Sinatra::Application
--- data.rb
+++ data.rb
@@ -15,6 +15,7 @@ get '/' do
   erb :towns
 end
 
+
 def create_town
   prefix = ['Sand', 'Scuttle', 'Carver', 'Co','Hopkin','New','Old', 'Ply', 'Attle']
   suffix =['well','harbor','ville','wich','scusset','bridge','hasset', 'ton','town','mouth','bourough']
