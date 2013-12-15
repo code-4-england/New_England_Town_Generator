42a71abe6d15a4f0f3fb59ac753ad40e9a49e592
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-12-14 22:04:55 -0500

    added pg and rake

--- Gemfile
+++ Gemfile
@@ -5,3 +5,9 @@ gem 'sinatra-reloader'
 group :development do
   gem 'pry'
 end
+
+gem 'pg'
+
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'rake'
--- data.rb
+++ data.rb
@@ -1,4 +1,10 @@
 require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+
+set :database, {adapter: 'postgresql',
+                database: 'stories_db',
+                host: 'localhost'}
 prefix = ['Sand', 'Scuttle', 'Carver', 'Co','Hopkin','New','Old', 'Ply', 'Attle']
 suffix =['well','harbor','ville','wich','scusset','bridge','hasset', 'ton','town','mouth','bourough']
 my_towns = []
@@ -6,4 +12,3 @@ for n in 0..5
 n = "#{prefix.sample}#{suffix.sample}"
 my_towns << n
 end
-binding.pry
